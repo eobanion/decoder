6dc217566922ea309fbc89f396d41e353736136e
eobanion <eobanion@gmail.com>
2020-01-23 15:04:43 -0800

    Add F-J conversions

--- decoder.rb
+++ decoder.rb
@@ -1,9 +1,14 @@
 KEY = {
   1 => 'A',
-  2 => 'B',
+  2 => 'A',
   3 => 'C',
   4 => 'D',
   5 => 'E',
+  6 => 'F',
+  7 => 'G',
+  8 => 'H',
+  9 => '*',
+  10 => 'J'
 }
 
 # Define a method that takes an array of numbers to decode.
@@ -13,8 +18,11 @@ def decode(numbers)
   numbers.each do |number|
     # Get the letter that corresponds to this number.
     letter = KEY[number]
-    # Add it onto the string.
-    text += letter
+    # If letter was found...
+    if letter != nil
+      # Add it onto the string.
+      text += letter
+    end
   end
   # Return the decoded string.
   return text
